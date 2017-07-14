537f3ab2802aaaa6160f7b0f1d9b73dd80c03d72
Nattapat Sukpootanan <Nattapat@Nattapats-MacBook-Pro-3.local>
2017-07-14 14:11:01 +0700

    maruko

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
   </div>
   <div style="position: fixed; z-index: -99; width: 100%; opacity: .3; height: 100%; background-color:rgba(255, 255, 255,0)">
     <iframe frameborder="0" height="100%" width="100%"
-      src="https://youtube.com/embed/dKrVegVI0Us?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1">
+      src="https://youtube.com/embed/F2HqJhiogfY?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1">
     </iframe>
   </div>
 
